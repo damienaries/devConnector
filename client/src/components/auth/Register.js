7aea1b185d55685c749dfe46ed57765678644327
Damien Aries <damien.aries@gmail.com>
2020-12-09 20:43:28 -0800

    Redux Set up & alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';  
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+
+const Register = ({ setAlert }) => {
     const [ formData, setFormData ] = useState({ 
         name: '',
         email: '',
@@ -16,28 +20,10 @@ const Register = () => {
      const onSubmit = async e => {
          e.preventDefault();
          if(password !== password2) {
-             console.log('passwods do not match')
+             setAlert('passwods do not match', 'danger');
+
          } else {
              console.log('Success!')
-            //  const newUser = {
-            //      name,
-            //      email,
-            //      password
-            //  }
-
-            // try {
-            //     const config = {
-            //         headers: {
-            //             'Content-Type': 'application/json'
-            //         }
-            //     }
-
-            //     const body = JSON.stringify(newUser);
-            //     const res = await axios.post('/api/users', body, config);
-            //     console.log(res.data);
-            // } catch (err) {
-            //     console.log(err.response.data);
-            // }
 
          }
      };
@@ -98,6 +84,10 @@ const Register = () => {
             
         </Fragment>
     )
+};
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
 }
 
-export default Register;
+export default connect(null, { setAlert })(Register);
