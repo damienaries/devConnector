1c89a3eeefdc0a7ea8eb292280796c0045de8e59
Damien Aries <damien.aries@gmail.com>
2020-12-14 17:03:36 -0800

    profile section finished. profile display erros to be fixed

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -51,7 +51,7 @@ const CreateProfile = ({ createProfile, history }) => {
         Create Your Profile
       </h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Let's get some information to make your
+        <i className="fas fa-user"></i> Please add some information to make your
         profile stand out
       </p>
       <small>* = required field</small>
